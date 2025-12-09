ec72d9c8fdc33a68c966ffa765c92d3141bdcc37
666-arch <hc18174419502@163.com>
2025-12-09 22:28:06 +0800

    add entities by class plan

---
+++ src/entities/plan.entities.ts
@@ -0,0 +1,31 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity({name: 'plan'})
+export class Plan {
+    @PrimaryGeneratedColumn()
+    id: number; //主键id
+
+    @Column({ length: 100 })
+    title: string; //攻略大标题
+
+    @Column({ type: 'int'})
+    days: number; //天数
+
+    @Column({ type: 'int', nullable: true})
+    budget: number | null; //预算
+
+    @Column({ type: 'date', nullable: true})
+    startTime: string | null; //开始时间
+
+    @Column({ type: 'date', nullable: true})
+    endTime: string | null; //结束时间
+
+    @Column({ type: 'text', nullable: true})
+    summary: string | null; //文本摘要（可能来自AI）
+
+    @Column({name: 'create_at'})
+    createAt: Date;
+
+    @Column({name: 'update_at'})
+    updateAt: Date;
+}
