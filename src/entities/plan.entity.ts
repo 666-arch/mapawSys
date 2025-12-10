2facd9728280b33b21dd67d8b40e3eef2aca9a07
MaxHuang <hc18174419502@163.com>
2025-12-10 14:22:16 +0800

    create tb map rule

--- src/entities/plan.entity.ts
+++ src/entities/plan.entity.ts
@@ -1,31 +1,35 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { User } from "./user.entity";
 
-@Entity({name: 'plan'})
+@Entity({ name: 'plan' })
 export class Plan {
-    @PrimaryGeneratedColumn({ type: 'bigint'})
+    @PrimaryGeneratedColumn({ type: 'bigint' })
     id: number; //主键id
+
+    @ManyToOne(() => User, u => u.plans, { nullable: false, onDelete: 'CASCADE' })
+    user: User; //多个Plan对应一个用户
 
     @Column({ length: 100 })
     title: string; //攻略大标题
 
-    @Column({ type: 'int'})
+    @Column({ type: 'int' })
     days: number; //天数
 
-    @Column({ type: 'int', nullable: true})
+    @Column({ type: 'int', nullable: true })
     budget: number | null; //预算
 
-    @Column({ type: 'date', nullable: true})
+    @Column({ type: 'date', nullable: true })
     startTime: string | null; //开始时间
 
-    @Column({ type: 'date', nullable: true})
+    @Column({ type: 'date', nullable: true })
     endTime: string | null; //结束时间
 
-    @Column({ type: 'text', nullable: true})
+    @Column({ type: 'text', nullable: true })
     summary: string | null; //文本摘要（可能来自AI）
 
-    @Column({name: 'create_at'})
+    @Column({ name: 'create_at' })
     createAt: Date;
 
-    @Column({name: 'update_at'})
+    @Column({ name: 'update_at' })
     updateAt: Date;
 }
--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,31 +1,35 @@
-import { Column, CreateDateColumn, Entity, Index, PrimaryGeneratedColumn } from "typeorm";
-@Entity({ name: 'user'})
+import { Column, CreateDateColumn, Entity, Index, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Plan } from "./plan.entity";
+@Entity({ name: 'user' })
 export class User {
-    @PrimaryGeneratedColumn({ type: 'bigint'})
+    @PrimaryGeneratedColumn({ type: 'bigint' })
     id: number; //用户主键id
-    
-    @Column({length: 50})
+
+    @OneToMany(() => Plan, (plan) => plan.user)
+    plans: Plan[]; //一个用户可以有多个 Plan
+
+    @Column({ length: 50 })
     username: string; //用户名
 
-    @Column({length: 255})
+    @Column({ length: 255 })
     password: string; //密码
 
-    @Column({length: 255, nullable: true})
+    @Column({ length: 255, nullable: true })
     phone: string | null; //手机号
 
     @Index()
-    @Column({length: 100, nullable: true})
+    @Column({ length: 100, nullable: true })
     email: string | null; //邮箱
 
-    @Column({ length: 10, nullable: true})
+    @Column({ length: 10, nullable: true })
     gender: string | null; //性别
 
-    @Column({ length: 255, nullable: true})
+    @Column({ length: 255, nullable: true })
     avatar: string | null; //头像
 
-    @CreateDateColumn({ name: 'create_at'})
+    @CreateDateColumn({ name: 'create_at' })
     createAt: Date; //创建时间
 
-    @CreateDateColumn({ name: 'update_at'})
+    @CreateDateColumn({ name: 'update_at' })
     updateAt: Date; //修改时间
 }
