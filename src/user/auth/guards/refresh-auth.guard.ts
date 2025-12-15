4dd1db0ff1a27269571486c635aed5d30ccd2f50
666-arch <hc18174419502@163.com>
2025-12-15 21:22:58 +0800

    create guard by user refresh token

---
+++ src/user/auth/guards/refresh-auth.guard.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+/**
+ * AuthGuard守卫 来自 passport
+ * 1.调用 passport.authenticate('jwt-refresh')
+ * 2.触发 RefreshStrategy
+ * 它只是一个触发器，通知 nest 接口需要通过何种 Strategy 来鉴权
+ */
+@Injectable()
+export class RefreshAuthGuard extends AuthGuard() {}
