22160346000a383013fa783946d42253064026f9
MaxHuang <hc18174419502@163.com>
2025-12-16 11:24:51 +0800

    AuthService config

--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,6 +1,44 @@
 import { Injectable } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { InjectRepository } from "@nestjs/typeorm";
+import { RefreshToken } from "src/entity/refresh-token.entity";
+import { User } from "src/entity/user.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class AuthService {
-    
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepo: Repository<User>,
+
+        @InjectRepository(RefreshToken)
+        private readonly refreshTokenRepo: Repository<RefreshToken>,
+
+        private readonly jwtService: JwtService,
+    ){}
+
+    /**
+     * 登录
+     */
+
+    /**
+     * validate userInfo
+     */
+
+    /**
+     * create Access Token
+     */
+
+    /**
+     * create Refresh Token
+     */
+
+    /**
+     * 刷新 Access Token
+     */
+
+    /**
+     * 登出（撤销 Refresh Token）
+     */
+
 }
