9d7e793e49cd68d56f981645a097f0fdb7353d63
MaxHuang <hc18174419502@163.com>
2025-12-16 17:03:39 +0800

    redis config and smsCode config add login module

---
+++ src/redis/redis.module.ts
@@ -0,0 +1,19 @@
+import { Global, Module } from "@nestjs/common";
+import Redis from 'ioredis';
+
+@Global() //全局，不用在每个 Module 重复导入
+@Module({
+    providers: [
+        {
+            provide: 'REDIS_CLIENT',
+            useFactory: () => {
+                return new Redis({
+                    host: '',
+                    port: 6379
+                });
+            },
+        },
+    ],
+    exports: ['REDIS_CLIENT']
+})
+export class RedisModule { }
--- src/user/auth/auth.service.ts
+++ src/user/auth/auth.service.ts
@@ -1,9 +1,12 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { Inject, Injectable, UnauthorizedException } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { InjectRepository } from "@nestjs/typeorm";
 import { RefreshToken } from "src/entity/refresh-token.entity";
 import { User } from "src/entity/user.entity";
 import { Repository } from "typeorm";
+import bcrypt from 'bcrypt'
+import { LoginDto } from "src/dto/user/login.dto";
+import { Redis } from "ioredis";
 
 @Injectable()
 export class AuthService {
@@ -15,22 +18,55 @@ export class AuthService {
         private readonly refreshTokenRepo: Repository<RefreshToken>,
 
         private readonly jwtService: JwtService,
-    ){}
+
+        @Inject('REDIS_CLIENT') //导入注册好的全局 Redis
+        private readonly redis: Redis,
+    ) { }
 
     /**
      * 登录
      */
 
     /**
-     * validate userInfo
+     * validate ByPassword
      */
-
-    private async validateUser(phone: string, password: string): Promise<User> {
+    private async validateByPassowrd(phone: string, password: string): Promise<User> {
         const user = await this.userRepo.findOne({
-            where: { phone },
-        })
+            where: { phone }
+        });
         if (!user) {
-            throw new UnauthorizedException('用户不存在')
+            throw new UnauthorizedException('用户不存在');
+        }
+        const matchPwd = await bcrypt.compare(password, user.password);
+        if (!matchPwd) {
+            throw new UnauthorizedException('用户密码错误');
+        }
+        return user;
+    }
+
+    /**
+     * validate BySmsCode
+     */
+    private async validateSmsCode(phone: string, code: string): Promise<User> {
+        const cacheCode = await this.redis.get(`sms:${phone}`);
+        if (!cacheCode) {
+            throw new UnauthorizedException('验证码已过期');
+        }
+        if (cacheCode !== code) {
+            throw new UnauthorizedException('验证码错误，请重试');
+        }
+        //验证码是一次性的，用完即删
+        await this.redis.del(`sms:${phone}`);
+
+        //验证手机号
+        let user = await this.userRepo.findOne({
+            where: { phone },
+        });
+
+        //如果 user 不存在则自动注册
+        if (!user) {
+            user = this.userRepo.create({ phone });
+            await this.userRepo.save(user);
         }
         return user;
     }
